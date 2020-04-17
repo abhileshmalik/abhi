93f230308bc4b79b3b2f924c80859e47c6229801
abhilesh <abhilesh.malik@tothenew.com>
2020-04-17 06:09:06 +0530

    review Response Entity

--- Online-Shopping/src/main/java/com/tothenew/project/OnlineShopping/controller/ProductReviewController.java
+++ Online-Shopping/src/main/java/com/tothenew/project/OnlineShopping/controller/ProductReviewController.java
@@ -5,6 +5,8 @@ import com.tothenew.project.OnlineShopping.entities.Customer;
 import com.tothenew.project.OnlineShopping.services.ProductReviewService;
 import com.tothenew.project.OnlineShopping.services.UserDaoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,12 +24,13 @@ public class ProductReviewController {
     private UserDaoService userDaoService;
 
     @PostMapping("/addreview/{product_id}")
-    public String addReview(@Valid @RequestBody ProductReviewDto productReviewDto, @PathVariable Long product_id){
+    public ResponseEntity<Object> addReview(@Valid @RequestBody ProductReviewDto productReviewDto, @PathVariable Long product_id){
         Customer customer = userDaoService.getLoggedInCustomer();
         Long customer_user_id = customer.getUser_id();
 
         String message = productReviewService.addReview(productReviewDto, customer_user_id, product_id);
-        return message;
+
+        return new ResponseEntity<>(message, HttpStatus.CREATED);
     }
 
 
