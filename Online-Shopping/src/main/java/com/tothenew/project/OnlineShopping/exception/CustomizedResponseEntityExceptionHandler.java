504eb3714659bd1a5d01989d6c2021ffbd014d6c
abhilesh <abhilesh.malik@tothenew.com>
2020-04-29 13:36:57 +0530

    Unique Exception

--- Online-Shopping/src/main/java/com/tothenew/project/OnlineShopping/exception/CustomizedResponseEntityExceptionHandler.java
+++ Online-Shopping/src/main/java/com/tothenew/project/OnlineShopping/exception/CustomizedResponseEntityExceptionHandler.java
@@ -23,6 +23,14 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
         return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    @ExceptionHandler(NotUniqueException.class)
+    public final ResponseEntity<Object> handleNotUniqueException(NotUniqueException ex, WebRequest request) {
+
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity(exceptionResponse, HttpStatus.NOT_ACCEPTABLE);
+    }
+
     @ExceptionHandler(UserNotFoundException.class)
     public final ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException ex, WebRequest request) {
 
